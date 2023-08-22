7e01167ba582fb74afe540273bbd0f150590525c
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-22 14:19:09 +0300

    complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def _init_(self):
+        self.total = 0
+        self.items = []
+        self.discount = 0
+        self.last_transaction = 0
+
+    def add_item(self, item, price):
+        self.items.append(item)
+        self.total += price
+        self.last_transaction = price
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            return f"Discount applied: ${discount_amount:.2f}"
+        else:
+            return "No discount applied."
+
+    def void_last_transaction(self):
+        if self.items:
+            self.total -= self.last_transaction
+            self.items.pop()
+            self.last_transaction = 0
+        else:
+            return "No items to void."
+
+    def get_total(self):
+        return f"Total: ${self.total:.2f}"
+
+    def get_items(self):
+        return self.items
+
